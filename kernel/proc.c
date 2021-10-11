202213b264b581e90170c80dd37ac126c90f1304
Martin Siran <xsiran@stuba.sk>
2021-10-11 16:50:54 +0200

    feat: implement acceleration of syscalls

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,14 @@ found:
     return 0;
   }
 
+  // Allocate a usyscall page, which will be readonly for user.
+  if((p->read_page = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->read_page->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -156,6 +164,9 @@ freeproc(struct proc *p)
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
+  if(p->read_page)
+    kfree((void *)p->read_page);
+  p->read_page = 0;
   p->sz = 0;
   p->pid = 0;
   p->parent = 0;
@@ -196,6 +207,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the readonly page for user
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->read_page), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +226,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
